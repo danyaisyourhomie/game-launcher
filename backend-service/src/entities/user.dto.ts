014e0c13b2ad4f615bced4d8beedf3b7bcd39580
Danil Kladnitskiy <daniil.kla@fora-soft.com>
2022-03-07 10:22:19 +0300

    remove unique

--- backend-service/src/entities/user.dto.ts
+++ backend-service/src/entities/user.dto.ts
@@ -8,13 +8,13 @@ import { BaseEntity } from './base_entity.dto';
 
 @Entity()
 export class User extends BaseEntity {
-  @Column({ unique: true })
+  @Column()
   nickname: string;
 
   @Column()
   password: string;
 
-  @Column({ unique: true })
+  @Column()
   uuid: string;
 
   @PrimaryGeneratedColumn('uuid')
